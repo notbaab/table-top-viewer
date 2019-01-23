86614f28321def4df3a8981455559d88af0e42f3
Erik Parreira <etinlb@gmail.com>
2019-01-23 09:44:16 -0800

    Fix bug with the component system not quite working for class objects

--- frontend/js/component-system.js
+++ frontend/js/component-system.js
@@ -9,31 +9,46 @@ function createEntity(properties, components) {
 
 function getAllMethodNames(obj) {
   let methods = new Set();
-  while (obj = Reflect.getPrototypeOf(obj)) {
-    let keys = Reflect.ownKeys(obj)
-    keys.forEach((k) => methods.add(k));
+  obj = Reflect.getPrototypeOf(obj);
+  while (obj.constructor.name !== "Object" ) {
+    let keys = Reflect.ownKeys(obj).filter(k => k !== "constructor");
+    keys.forEach( k => methods.add(k));
+    obj = Reflect.getPrototypeOf(obj);
   }
+
   return methods;
 }
 
 function createEntityFromTemplate(templateObj) {
   /**
-   * A way to fake inheritance with composition.
+   * A way to fake inheritance with composition. I'm not sure How much
+   * I like this now
    */
 
   var prop;
-  console.log(templateObj);
   // Merge in the properties dict. Why is this here again?
   for (prop in templateObj.properties) {
     templateObj[prop] = templateObj.properties[prop];
   }
 
   templateObj.components.forEach(function(component) {
+    // first, get the first level method from the component
+    let componentMethods = getAllMethodNames(component);
+
+    for(let method of componentMethods.values()) {
+      // Base obj doesn't have the component method, add it to the base
+      // TODO: This could stomp on properties with the same name.
+      if (typeof templateObj[method] !== 'function') {
+        templateObj[method] = component[method];
+      }
+      // else if it does have the property, it will be handled in the
+      // over ride step below
+    }
+
     // loop over any properties
     for (prop in component) {
       if (templateObj.hasOwnProperty(prop)) {
         // check overriding
-        console.log("has " + prop);
         if(templateObj.overRide !== undefined && templateObj.overRide[prop] === component.type ){
           console.log("setting precedence" + prop)
           templateObj[prop] = component[prop];
@@ -49,7 +64,6 @@ function createEntityFromTemplate(templateObj) {
     }
 
     // loop over all the methods
-    let componentMethods = getAllMethodNames(component);
     for (overload in templateObj.overload) {
       if(component[overload] === undefined) {
         continue;
@@ -59,5 +73,6 @@ function createEntityFromTemplate(templateObj) {
     }
   });
 
+  console.log(templateObj);
   return templateObj;
 }
